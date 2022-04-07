b69e0dd28b3a80474eb88a1a668b35c3b44c584d
Your Name <leobarrientos02@gmail.com>
2022-04-07 04:13:24 -0400

    authentication system working

---
+++ src/test/java/com/revature/Driver.java
@@ -0,0 +1,108 @@
+package com.revature;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Driver {
+    public static ArrayList<User> Users = new ArrayList<User>();
+    public static int id = -1;
+    public static int choice;
+    public static Scanner scanner = new Scanner(System.in);
+    public static String loginEmail;
+    public static String loginPassword;
+
+    public static void main(String[] args){
+        System.out.println("---------------------");
+        System.out.println("WELCOME TO THE SYSTEM");
+        System.out.println("---------------------");
+        lp : while(true){
+            System.out.println("");
+            System.out.println("Choose an option");
+            Menu();
+            choice = scanner.nextInt();
+
+            switch(choice){
+                case 1:
+                    if(Users.isEmpty()){
+                        System.out.println("There are currently no users, please register");
+                        break;
+                    } //else if( loginEmail.equals(Users.get(0).getEmail()) && loginPassword.equals(Users.get(0).getPassword()))
+                        //System.out.println("Welcome " + Users.get(0).getFirstName() + " " + Users.get(0).getLastName());
+                    login();
+
+
+                    ArrayList<Boolean> result = new ArrayList<Boolean>();
+                    for(User user : Users){
+                        if(user.getEmail().contains(loginEmail) && user.getPassword().contains(loginPassword)){
+                            result.add(true);
+                        }else{
+                            result.add(false);
+                        }
+                    }
+                    //System.out.println(result.indexOf(true));
+                    id = result.indexOf(true);
+
+                    if( id != -1 ){
+                        System.out.println("Welcome " + Users.get(id).getFirstName() + " " + Users.get(id).getLastName());
+                    }else {
+                        System.out.println("Account not found.");
+                    }
+
+                    break;
+                case 2:
+                    register();
+                    break;
+                case 3:
+                    System.out.println("Goodbye");
+                    break lp;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+
+    public static void Menu(){
+        System.out.println("1: Login");
+        System.out.println("2: Register ");
+        System.out.println("3: Exit");
+    }
+
+    public static void login(){
+
+        System.out.println("Login");
+        System.out.println("-----");
+        System.out.println("Please enter your email");
+        loginEmail = scanner.next();
+        System.out.println("Please enter your password");
+        loginPassword = scanner.next();
+
+    }
+
+    public static void register(){
+        User user = new User();
+        String firstName;
+        String lastName;
+        String email;
+        String password;
+
+        System.out.println("Register");
+        System.out.println("--------");
+        System.out.println("Please enter your first name:");
+        firstName = scanner.next();
+        System.out.println("Please enter your last name:");
+        lastName = scanner.next();
+        System.out.println("Please enter a valid email:");
+        email = scanner.next();
+        System.out.println("Please enter a strong password:");
+        password = scanner.next();
+
+
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        Users.add(user);
+    }
+
+    
+}
---
+++ src/test/java/com/revature/User.java
@@ -0,0 +1,62 @@
+package com.revature;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String accountType;
+
+    // constructors
+    public User(){
+
+    }
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        accountType = "user";
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
